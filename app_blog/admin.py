073189ba92f475af12f7b1d6781ed2a74604d935
bhezcreator <bhezcreator@gmail.com>
2023-12-26 21:50:54 +0100

    Ajout de modele et affichage dans le template

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Blog
+
 # Register your models here.
+# Recupération de données pour l'afficher dans le panneau d'admin
+admin.site.register(Blog)
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 # Affichage de la page accueil du module blog
 def indexView(request):
-    return render(request, 'blog/index.html')
+
+    list_blogs = Blog.objects.all()
+
+    context = {
+        'list_blogs' : list_blogs
+    }
+    return render(request, 'blog/index.html',context)
 
 
 # Affichage de la page contact du module blog
